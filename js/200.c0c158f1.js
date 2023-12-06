2b538a8459bca0e00190cf5010462f201404dbcd
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-12-06 22:58:10 +0200

    Updates

---
+++ js/200.c0c158f1.js
@@ -0,0 +1,97 @@
+"use strict";(self["webpackChunkmy_cv"]=self["webpackChunkmy_cv"]||[]).push([[200],{4237:function(e,t,n){n.r(t),n.d(t,{default:function(){return C}});var i=n(3396);function a(e,t,n,a,o,r){const p=(0,i.up)("MainComponent");return(0,i.wg)(),(0,i.j4)(p)}var o=n(7139),r=n.p+"img/my-photo.76d9617c.jpg";const p=e=>((0,i.dD)("data-v-7ed7582f"),e=e(),(0,i.Cn)(),e),d=p((()=>(0,i._)("img",{class:"photo",src:r,width:"380",alt:"Dmytro Smirnov photo"},null,-1)));function m(e,t,n,a,r,p){const m=(0,i.up)("ImgContainer"),l=(0,i.up)("HeroName"),s=(0,i.up)("HeroWork"),c=(0,i.up)("HeroDescription"),u=(0,i.up)("router-link"),x=(0,i.up)("HeroInfoContainer"),h=(0,i.up)("HeroContainer"),g=(0,i.up)("HeroSection");return(0,i.wg)(),(0,i.j4)(g,{style:(0,o.j5)(p.themeStyles)},{default:(0,i.w5)((()=>[(0,i.Wm)(h,{class:"container"},{default:(0,i.w5)((()=>[(0,i.Wm)(m,null,{default:(0,i.w5)((()=>[d])),_:1}),(0,i.Wm)(x,null,{default:(0,i.w5)((()=>[(0,i.Wm)(l,null,{default:(0,i.w5)((()=>[(0,i.Uk)((0,o.zw)(e.$t("name_main")),1)])),_:1}),(0,i.Wm)(s,null,{default:(0,i.w5)((()=>[(0,i.Uk)((0,o.zw)(e.$t("post_main")),1)])),_:1}),(0,i.Wm)(c,null,{default:(0,i.w5)((()=>[(0,i.Uk)((0,o.zw)(e.$t("title_main")),1)])),_:1}),(0,i.Wm)(u,{class:"hero-link",to:"/about"},{default:(0,i.w5)((()=>[(0,i.Uk)((0,o.zw)(e.$t("button_main")),1)])),_:1})])),_:1})])),_:1})])),_:1},8,["style"])}var l=n(7757);const s=l.ZP.div`
+  padding-top: 80px;
+  padding-bottom: 30px;
+
+  @media screen and (min-width: 768px) {
+    height: 100vh;
+  }
+`,c=l.ZP.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and (min-width: 768px) {
+    flex-direction: row;
+    gap: 40px;
+
+    animation-name: fadeIn;
+    animation-duration: 1.5s;
+  }
+  @media screen and (min-width: 1200px) {
+    gap: 0;
+    display: flex;
+  }
+`,u=l.ZP.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+
+  @media screen and (min-width: 1200px) {
+    width: 50%;
+  }
+`,x=l.ZP.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+
+  text-align: center;
+
+  @media screen and (min-width: 768px) {
+    display: block;
+    margin-top: 0px;
+    width: 720px;
+
+    text-align: left;
+  }
+  @media screen and (min-width: 1200px) {
+    width: 800px;
+  }
+`,h=l.ZP.p`
+  margin-bottom: 10px;
+
+  font-size: 40px;
+  font-weight: 700;
+  line-height: 1.2;
+  letter-spacing: 0px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 10px;
+  }
+  @media screen and (min-width: 1200px) {
+    margin: 0px;
+
+    font-size: 70px;
+    font-weight: 700;
+    line-height: 33px;
+    letter-spacing: 0px;
+  }
+`,g=l.ZP.p`
+  margin-bottom: 25px;
+
+  font-size: 30px;
+  font-weight: 500;
+  line-height: 1.5;
+  letter-spacing: 0px;
+
+  @media screen and (min-width: 1200px) {
+    margin-bottom: 40px;
+
+    font-size: 40px;
+    font-weight: 500;
+    line-height: 130px;
+    letter-spacing: 0px;
+    margin: 0px;
+  }
+`,f=l.ZP.p`
+  margin: 0px;
+
+  padding-bottom: 30px;
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 30px;
+  letter-spacing: 0px;
+`;var w={components:{HeroSection:s,HeroContainer:c,ImgContainer:u,HeroInfoContainer:x,HeroName:h,HeroWork:g,HeroDescription:f},computed:{themeStyles(){return{backgroundColor:this.$store.state.isThemeDark?"rgba(40, 40, 40, 0.8)":"rgba(255, 255, 255)",backdropFilter:"blur(10px)",color:this.$store.state.isThemeDark?"white":"black"}}}},k=n(89);const b=(0,k.Z)(w,[["render",m],["__scopeId","data-v-7ed7582f"]]);var _=b,v={name:"MainPage",components:{MainComponent:_}};const y=(0,k.Z)(v,[["render",a]]);var C=y}}]);
+//# sourceMappingURL=200.c0c158f1.js.map
