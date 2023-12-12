ff767b44b327e3803127933bf9b9e9638c6fddd1
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-12-12 12:23:55 +0200

    new

--- src/components/Modal/ModalWindow.styled.js
+++ src/components/Modal/ModalWindow.styled.js
@@ -1,3 +1,34 @@
 import styled from "vue3-styled-components";
 
-export const HeroSection = styled.div``;
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 10px;
+`;
+
+export const Close = styled.div`
+  display: flex;
+  padding: 3px;
+  border-radius: 50%;
+
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+`;
+
+export const ImgContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+export const Image = styled.div`
+  border-radius: 20px;
+`;
+
+export const NameContainer = styled.div``;
+
+export const AddingContainer = styled.div``;
+
+export const TechSkill = styled.p``;
