bce506d3672bb7af9006ac4e4e29e08583872a14
Lucky13identify <108291368+Lucky13identify@users.noreply.github.com>
2023-12-13 10:17:46 +0200

    new

--- src/components/ModalMenu/ModalMenu.styled.js
+++ src/components/ModalMenu/ModalMenu.styled.js
@@ -1,7 +1,25 @@
 import styled from "vue3-styled-components";
 
-export const ButtonContainer = styled.div``;
+export const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin-bottom: 10px;
+`;
 
-export const Close = styled.button``;
+export const Close = styled.button`
+  display: flex;
+  padding: 3px;
+  border-radius: 50%;
 
-export const List = styled.ul``;
+  cursor: pointer;
+  border: none;
+  background-color: transparent;
+`;
+
+export const List = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  margin-right: auto;
+  width: 200px;
+`;
